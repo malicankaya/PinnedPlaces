e5765a05d9d0d4f183d8b2370aa1be98f317cf47
malicankaya <malicankaya97@gmail.com>
2022-09-30 01:20:05 +0300

    Adding Calendar Picker And Populate Our Date Edit Text

--- app/src/main/java/com/malicankaya/pinnedplaces/AddPlaceActivity.kt
+++ app/src/main/java/com/malicankaya/pinnedplaces/AddPlaceActivity.kt
@@ -1,11 +1,19 @@
 package com.malicankaya.pinnedplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.malicankaya.pinnedplaces.databinding.ActivityAddPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddPlaceActivity : AppCompatActivity() {
+class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
     private var binding: ActivityAddPlaceBinding? = null
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddPlaceBinding.inflate(layoutInflater)
@@ -17,10 +25,43 @@ class AddPlaceActivity : AppCompatActivity() {
         binding?.toolbarAddPlace?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        binding?.etDate?.setOnClickListener(this)
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            setDateEditText()
+        }
+
+    }
+
+    private fun setDateEditText(){
+        val format = "dd.mm.yyyy"
+        val sdf = SimpleDateFormat(format,Locale.getDefault())
+
+        binding?.etDate?.setText(sdf.format(cal.time).toString())
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         binding = null
     }
+
+
 }
