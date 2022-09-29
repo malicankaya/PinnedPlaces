a572f766face76286e84d2ec5a9cffae29c673ce
malicankaya <malicankaya97@gmail.com>
2022-09-30 00:54:39 +0300

    Adding The Add HappyPlaceActivity and using dimens, string, color

--- app/src/main/java/com/malicankaya/pinnedplaces/AddPlaceActivity.kt
+++ app/src/main/java/com/malicankaya/pinnedplaces/AddPlaceActivity.kt
@@ -2,10 +2,25 @@ package com.malicankaya.pinnedplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.malicankaya.pinnedplaces.databinding.ActivityAddPlaceBinding
 
 class AddPlaceActivity : AppCompatActivity() {
+    private var binding: ActivityAddPlaceBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_place)
+        binding = ActivityAddPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
--- app/src/main/java/com/malicankaya/pinnedplaces/MainActivity.kt
+++ app/src/main/java/com/malicankaya/pinnedplaces/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.malicankaya.pinnedplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.malicankaya.pinnedplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddPlace?.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddPlaceActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
