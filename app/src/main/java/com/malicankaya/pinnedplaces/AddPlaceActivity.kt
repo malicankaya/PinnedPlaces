5b3ab2bc29b0dd0cc523c5773fa367d976e7188f
malicankaya <malicankaya97@gmail.com>
2022-09-29 03:39:38 +0300

    Setting Up The Happy Places App

---
+++ app/src/main/java/com/malicankaya/pinnedplaces/AddPlaceActivity.kt
@@ -0,0 +1,11 @@
+package com.malicankaya.pinnedplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_place)
+    }
+}
