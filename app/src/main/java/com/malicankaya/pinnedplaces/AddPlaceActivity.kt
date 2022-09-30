38a0cff467028aab8cecf938ec986d67cff2087d
malicankaya <malicankaya97@gmail.com>
2022-09-30 03:11:28 +0300

    adding permission handling

--- app/src/main/java/com/malicankaya/pinnedplaces/AddPlaceActivity.kt
+++ app/src/main/java/com/malicankaya/pinnedplaces/AddPlaceActivity.kt
@@ -1,17 +1,44 @@
 package com.malicankaya.pinnedplaces
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.malicankaya.pinnedplaces.databinding.ActivityAddPlaceBinding
+import java.security.Permission
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
     private var binding: ActivityAddPlaceBinding? = null
-
     private var cal = Calendar.getInstance()
+
+    private var cameraPermission: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted) {
+                Toast.makeText(this, "Permission denied for camera.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    private var readPermissions: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted) {
+                Toast.makeText(this, "Permission denied for choose photo.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,11 +62,71 @@ class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
             setDateEditText()
         }
 
+        binding?.tvAddImage?.setOnClickListener {
+            selectImage()
+        }
+
     }
 
-    private fun setDateEditText(){
+    private fun selectImage() {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle("Select action")
+
+        val pictureDialogItems = arrayOf("Select photo from Gallery", "Capture photo from Camera")
+        builder.setItems(pictureDialogItems) { _, which ->
+            when (which) {
+                0 -> choosePhotoFromGallery()
+                1 -> TODO()
+            }
+        }
+
+        builder.create().show()
+    }
+
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("GOTO SETTINGS") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        builder.create().show()
+    }
+
+    private fun choosePhotoFromGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+            && shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)
+        ) {
+            showRationaleDialog(
+                "Selecting photo requires to access photos and media",
+                "Selecting photo cannot be used because accessing photos and media is denied."
+            )
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_DENIED
+            ) {
+                readPermissions.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }else{
+
+            }
+
+        }
+    }
+
+    private fun setDateEditText() {
         val format = "dd.mm.yyyy"
-        val sdf = SimpleDateFormat(format,Locale.getDefault())
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
 
         binding?.etDate?.setText(sdf.format(cal.time).toString())
     }
