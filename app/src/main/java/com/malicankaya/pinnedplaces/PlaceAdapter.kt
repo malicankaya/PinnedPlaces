1f85bc86a42fecc01c21c5fb828d5b6e18d743df
malicankaya <malicankaya97@gmail.com>
2022-10-03 01:27:38 +0300

    Setting up adapter

---
+++ app/src/main/java/com/malicankaya/pinnedplaces/PlaceAdapter.kt
@@ -0,0 +1,31 @@
+package com.malicankaya.pinnedplaces
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.malicankaya.pinnedplaces.databinding.ItemPlaceBinding
+import com.malicankaya.pinnedplaces.models.PlaceEntity
+
+class PlaceAdapter(private val placeList: List<PlaceEntity>):RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    class ViewHolder(val itemBinding: ItemPlaceBinding): RecyclerView.ViewHolder(itemBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemPlaceBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemBinding.sivPlaceImage.setImageURI(Uri.parse(placeList[position].image))
+        holder.itemBinding.tvTitle.text = placeList[position].title
+        holder.itemBinding.tvDescription.text = placeList[position].description
+    }
+
+    override fun getItemCount(): Int {
+        return placeList.size
+    }
+}
